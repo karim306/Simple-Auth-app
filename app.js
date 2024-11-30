35be7b86f3d31ac8a4f0fc5f9d16e512a2eb9c15
Kareem <kareem@gmail.com>
2024-11-30 15:10:12 +0200

    Connect to Mongoose

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
-
-
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded( { extended: true } ));
 
-app.use('/' , (req , res) => {
+//connect to mongoose
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log( "connecting to Datbase...");
+}).catch((err )=>{
+
+     console.l0g(err);
+});
+
+app.get('/' , (req , res) => {
     res.json({message: "hello world!"});
 })
 
@@ -16,4 +30,6 @@ app.use('/' , (req , res) => {
 
 
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
