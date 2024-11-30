4b225b9ed5698f8fd41735d495ddb234a225c08e
Kareem <kareem@gmail.com>
2024-11-30 16:58:09 +0200

    Modfied auth controller handkled hash func

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const {signupSchema} = require('../middlewares/validator')
-
+const User  = require ('../models/usersModels');
+const {dohash} = require('../utils/hashing');
 
 
 
@@ -11,13 +12,22 @@ exports.signup = async (req , res ) =>{
        if(error){
         return res.status(401).json({success:false ,  message: error.details[0].message});
        }
-       
+
         const existinguser = await User.findOne({email});
         if(existinguser){
             return res.status(401).json({success:false ,  message: 'User already exists'});
         }
+       
+        const hashedPassword = await dohash(password , 12 );
+        const newUser = new User({
+            email,
+            password: hashedPassword
+        })
+        const result = await newUser.save();
 
-        
+        result.password = undefined;
+        res.status(201).json({success:true ,  message: 'User created successfully', data: result});
+
     } catch(error){
         console.log(error);
     }
