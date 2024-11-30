2003dce7a63b01a17103b7155626e56f8d61ada9
Kareem <kareem@gmail.com>
2024-11-30 15:30:12 +0200

    Modified post schema model

--- models/postModel.js
+++ models/postModel.js
@@ -3,26 +3,32 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true , 
+        trim : true
     },
     content: {
         type: String,
-        required: true
+        required: true , 
+        trim : true
     },
-    author: {
+    userID: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-    comments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Comment'
-    }],
+    // comments: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Comment'
+    // }],
     createdAt: {
         type: Date,
         default: Date.now
     },
     updatedAt: {
         type: Date
-    }
+    } , 
+    timestamps : true
+
 })
+
+module.exports= mongoose.model("Post" , postSchema); 
