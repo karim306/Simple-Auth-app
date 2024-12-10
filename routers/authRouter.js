b7db330f4319fa05e6b14f73435192ebd4a131c4
Kareem <kareem@gmail.com>
2024-12-11 01:00:02 +0200

    added identfier token

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-
+const { identifier } = require('../middlewares/identification');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -9,13 +9,13 @@ router.post('/signout',  authController.signout);
 
 router.patch(
 	'/send-verification-code',
-	
+	identifier,
 	authController.sendVerificationCode
 );
 
 router.patch(
 	'/verify-verification-code',
-	
+	identifier,
 	authController.verifyVerificationCode
 );
 
